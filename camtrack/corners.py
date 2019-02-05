ca4e2253624e02362ba4527ffa7f71dc5db5770c
Egor Shcherbin <javamanse@ya.ru>
2019-02-05 15:58:15 +0300

    Added corners from blocks

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -62,13 +62,74 @@ def _get_new_points_global(image, st_global_params, cur_points):
                                      useHarrisDetector=False)
     sizes = np.ones(len(points), dtype=np.int) * st_global_params['blockSize']
 
+    if points is not None:
+        points, sizes = points.reshape(-1, 2), sizes.reshape(-1, 1)
+    return points, sizes
+
+
+def _get_new_points_blocks(image, n_rows, n_cols,
+                           st_blocks_params, cur_points):
+    mask_global = np.ones(image.shape, dtype=np.uint8)
+    if cur_points is not None:
+        for point in cur_points:
+            cv2.circle(mask_global,
+                       tuple(point),
+                       st_blocks_params['minDistance'],
+                       color=0,
+                       thickness=cv2.FILLED)
+
+    points_blocks_list = []
+    xs = np.linspace(0, image.shape[0], num=n_cols + 1, dtype=np.int)
+    ys = np.linspace(0, image.shape[1], num=n_rows + 1, dtype=np.int)
+    for xa, xb in zip(xs[:-1], xs[1:]):
+        for ya, yb in zip(ys[:-1], ys[1:]):
+            mask_block = cv2.rectangle(np.zeros_like(mask_global),
+                                       (xa, ya),
+                                       (xb, yb),
+                                       color=1,
+                                       thickness=cv2.FILLED)
+            points_block = cv2.goodFeaturesToTrack(
+                image,
+                **st_blocks_params,
+                mask=mask_global & mask_block,
+                useHarrisDetector=False
+            )
+            if points_block is not None:
+                points_blocks_list.append(points_block)
+
+    if not points_blocks_list:
+        return None, None
+    points = np.concatenate(points_blocks_list, axis=0)
+    sizes = np.ones(len(points), dtype=np.int) * st_blocks_params['blockSize']
+    points, sizes = points.reshape(-1, 2), sizes.reshape(-1, 1)
     return points, sizes
 
 
 def get_new_points(image, config, cur_points=None):
-    return _get_new_points_global(image,
-                                  config['st_global_params'],
-                                  cur_points)
+    new_points_global, new_sizes_global = \
+        _get_new_points_global(image,
+                               config['st_global_params'],
+                               cur_points)
+
+    new_cur_points = np.concatenate([cur_points, new_points_global], axis=0) \
+        if cur_points is not None and new_points_global is not None \
+        else cur_points if new_points_global is None else new_points_global
+    new_points_blocks, new_sizes_blocks = \
+        _get_new_points_blocks(image,
+                               config['blocks_n_rows'],
+                               config['blocks_n_cols'],
+                               config['st_blocks_params'],
+                               new_cur_points)
+
+    new_points_list, new_sizes_list = [], []
+    if new_points_global is not None:
+        new_points_list.append(new_points_global)
+        new_sizes_list.append(new_sizes_global)
+    if new_points_blocks is not None:
+        new_points_list.append(new_points_blocks)
+        new_sizes_list.append(new_sizes_blocks)
+    return np.concatenate(new_points_list, axis=0), \
+        np.concatenate(new_sizes_list, axis=0)
 
 
 def _build_impl(frame_sequence: pims.FramesSequence,
@@ -91,7 +152,9 @@ def _build_impl(frame_sequence: pims.FramesSequence,
         corners = FrameCorners(corners.ids, next_points, corners.sizes)
         corners = filter_frame_corners(corners, np.squeeze(status == 1))
 
-        new_points, new_sizes = get_new_points(image_1, config, corners.points)
+        new_points, new_sizes = get_new_points(image_1,
+                                               config,
+                                               corners.points)
         # print(maxCorners - len(corners.points), len(new_points))
         if new_points is not None:
             new_ids = \
@@ -101,9 +164,9 @@ def _build_impl(frame_sequence: pims.FramesSequence,
             corners = FrameCorners(
                 np.concatenate([corners.ids, new_ids.reshape(-1, 1)],
                                axis=0),
-                np.concatenate([corners.points, new_points.reshape(-1, 2)],
+                np.concatenate([corners.points, new_points],
                                axis=0),
-                np.concatenate([corners.sizes, new_sizes.reshape(-1, 1)],
+                np.concatenate([corners.sizes, new_sizes],
                                axis=0),
             )
 
