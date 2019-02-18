3ef4a680d1aa2bf4e468e125343051a871d2a04b
Egor Shcherbin <javamanse@ya.ru>
2019-02-18 20:41:49 +0700

    Added frustrum

--- camtrack/renderer.py
+++ camtrack/renderer.py
@@ -16,6 +16,30 @@ import data3d
 
 
 _CL2GL = np.diag([1, -1, -1, 1]).astype(np.float32)
+_CUBE = np.array([
+    [-1, -1, -1, 1],
+    [ 1, -1, -1, 1],
+    [ 1,  1, -1, 1],
+    [-1,  1, -1, 1],
+    [-1, -1,  1, 1],
+    [ 1, -1,  1, 1],
+    [ 1,  1,  1, 1],
+    [-1,  1,  1, 1],
+], dtype=np.float32)
+_EDGES = [
+    0, 1,
+    1, 2,
+    2, 3,
+    3, 0,
+    4, 5,
+    5, 6,
+    6, 7,
+    7, 4,
+    0, 4,
+    1, 5,
+    2, 6,
+    3, 7,
+]
 
 
 def _build_color_program():
@@ -82,7 +106,7 @@ class CameraTrackRenderer:
             vbo.VBO(np.array([[0, 0, 1]] * len(tracked_cam_track)).astype(np.float32))
 
         self._cam_point_color_buffer_object = vbo.VBO(np.array([1, 0, 0], dtype=np.float32))
-        self._cam_frustrum_color_buffer_object = vbo.VBO(np.array([1, 1, 0], dtype=np.float32))
+        self._cam_frustrum_color_buffer_object = vbo.VBO(np.array([[1, 1, 0]] * len(_EDGES), dtype=np.float32))
 
         self._color_program = _build_color_program()
 
@@ -90,8 +114,12 @@ class CameraTrackRenderer:
         GL.glEnable(GL.GL_DEPTH_TEST)
 
     @staticmethod
+    def _build_transform(tr, rot):
+        return np.block([[rot, tr.reshape(-1, 1)], [0, 0, 0, 1]]).astype(np.float32)
+
+    @staticmethod
     def _build_view(tr, rot):
-        return np.linalg.inv(np.block([[rot, tr.reshape(-1, 1)], [0, 0, 0, 1]])).astype(np.float32)
+        return np.linalg.inv(CameraTrackRenderer._build_transform(tr, rot))
 
     @staticmethod
     def _build_proj(fov_y, ratio, near, far):
@@ -122,31 +150,31 @@ class CameraTrackRenderer:
         tracked_cam_track_pos = int(tracked_cam_track_pos_float)
 
         cur_ratio = GLUT.glutGet(GLUT.GLUT_WINDOW_WIDTH) / GLUT.glutGet(GLUT.GLUT_WINDOW_HEIGHT)
-        mvp = self._build_proj(camera_fov_y, cur_ratio, 0.001, 50) \
+        vp = self._build_proj(camera_fov_y, cur_ratio, 0.001, 50) \
             @ self._build_view(camera_tr_vec, camera_rot_mat) \
             @ _CL2GL
-        # print(camera_tr_vec)
-        # print(camera_rot_mat)
-        # print(self._build_view(camera_tr_vec, camera_rot_mat))
-        # print(self.normalize(mvp @ np.array([0, 0, 0, 1])))
-        # view_ = self._build_view(camera_tr_vec, camera_rot_mat) @ self._CL2GL @ np.array([1, 1, 1, 1])
-        # print(view_)
-        # print(np.linalg.norm(view_[:3]))
 
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
 
-        self._render_cloud(mvp)
-        self._render_track(mvp)
-        self._render_cam_point(mvp, self._cam_track[tracked_cam_track_pos].t_vec)
+        self._render_cloud(vp)
+        self._render_track(vp)
+        self._render_cam_point(vp,
+                               self._cam_track[tracked_cam_track_pos].t_vec)
+        self._render_cam_frustrum(vp,
+                                  self._cam_track[tracked_cam_track_pos].t_vec,
+                                  self._cam_track[tracked_cam_track_pos].r_mat)
+        #
+        # self._render_cam_point(vp, np.array([0, 0, 0]))
+        # self._render_cam_frustrum(vp, np.array([0, 0, 0]), np.eye(3))
 
         GLUT.glutSwapBuffers()
 
-    def _render_common(self, mvp, buffers, draw_mode, draw_cnt):
+    def _render_common(self, vp, buffers, draw_mode, draw_cnt):
         shaders.glUseProgram(self._color_program)
 
         GL.glUniformMatrix4fv(
             GL.glGetUniformLocation(self._color_program, 'mvp'),
-            1, True, mvp)
+            1, True, vp)
 
         for attrib, buffer in buffers:
             buffer.bind()
@@ -163,24 +191,37 @@ class CameraTrackRenderer:
 
         shaders.glUseProgram(0)
 
-    def _render_cloud(self, mvp):
+    def _render_cloud(self, vp):
         buffers = [
             ('position', self._points_pos_buffer_object),
             ('color_in', self._points_color_buffer_object)
         ]
-        self._render_common(mvp, buffers, GL.GL_POINTS,
+        self._render_common(vp, buffers, GL.GL_POINTS,
                             self._points_pos_buffer_object.size // 3)
 
-    def _render_track(self, mvp):
+    def _render_track(self, vp):
         buffers = [
             ('position', self._track_pos_buffer_object),
             ('color_in', self._track_color_buffer_object)
         ]
-        self._render_common(mvp, buffers, GL.GL_LINE_STRIP, len(self._cam_track))
+        self._render_common(vp, buffers, GL.GL_LINE_STRIP, len(self._cam_track))
 
-    def _render_cam_point(self, mvp, cam_t_vec):
+    def _render_cam_point(self, vp, cam_t_vec):
         buffers = [
             ('position', vbo.VBO(np.array(cam_t_vec, dtype=np.float32))),
             ('color_in', self._cam_point_color_buffer_object)
         ]
-        self._render_common(mvp, buffers, GL.GL_POINTS, 1)
+        self._render_common(vp, buffers, GL.GL_POINTS, 1)
+
+    def _render_cam_frustrum(self, vp, cam_t_vec, cam_r_mat):
+        frustrum = (self._build_transform(cam_t_vec, cam_r_mat)
+                    @ _CL2GL
+                    @ np.linalg.inv(self._build_proj(self._cam_fov_y,
+                                                     self._cam_ratio, 2, 50))
+                    @ _CUBE.T).astype(np.float32)
+        frustrum = (frustrum[:-1] / frustrum[-1]).T
+        buffers = [
+            ('position', vbo.VBO(frustrum[_EDGES])),
+            ('color_in', self._cam_frustrum_color_buffer_object)
+        ]
+        self._render_common(vp, buffers, GL.GL_LINES, len(_EDGES))
